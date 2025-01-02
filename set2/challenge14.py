941db8bdfe5f62dc64022f0bca57167e15e55eae
Maurits <mauritsvdsteur@live.com>
2025-01-02 18:27:14 +0100

    c14

--- set2/challenge14.py
+++ set2/challenge14.py
@@ -9,8 +9,7 @@ suffix_bin = base642bin(
 
 
 def encryption_oracle(b_in: bytes, key: bytes) -> bytes:
-    # binary = prefix_bin + b_in + suffix_bin
-    binary = b_in + suffix_bin
+    binary = prefix_bin + b_in + suffix_bin
 
     encrypted = encrypt_aes_ecb(binary, key)
 
@@ -18,13 +17,13 @@ def encryption_oracle(b_in: bytes, key: bytes) -> bytes:
 
 
 def find_key_size(key):
-    # start4: smallest key size=2, so 2*2=4 to have two b2b
+    # smallest key size to try, otherwise theres a random chance of
+    # getting b2b same bytes, annoying to have to double check and what not
     smallest_key_size = 4
     for input_size in range(smallest_key_size * 2, 100):
         binary = bytes([ord("A") for _ in range(input_size)])
         encrypt = encryption_oracle(binary, key)
 
-        # key size is prob not 1, otherwise too much chance of b2b bytes
         for key_size in range(smallest_key_size, int(input_size / 2) + 1):
             last_block = None
             blocks = [encrypt[i:i + key_size] for i in range(0, len(encrypt), key_size)]
@@ -42,27 +41,28 @@ MAX_ASCII = 128
 # if we know/assume the plaintext just has 8bit ascii then it should mostly work
 # TODO padding at the end?
 def find_all_bytes(key_size, key, unknown_byte_size_in, offset):
-    unknown_byte_index = unknown_byte_size_in + (key_size - (unknown_byte_size_in % key_size))
-    unknown_byte_size = offset + unknown_byte_index
+    unknown_byte_size = offset + unknown_byte_size_in + (key_size - (unknown_byte_size_in % key_size))
     byte_padding = "A"
 
-    matches = [[] for _ in range(unknown_byte_size)]
-    matches_index = [0] * unknown_byte_size
+    matches = [[] for _ in range(unknown_byte_size_in)]
+    matches_index = [0] * unknown_byte_size_in
     b = 1
-    while b <= unknown_byte_size:
+    while b <= unknown_byte_size_in:
         print("Byte:", b, "/", unknown_byte_size)
         print("".join([chr(matches[i][matches_index[i]]) for i in range(b - 1)]))
         working_block = bytearray([ord(byte_padding) for _ in range(unknown_byte_size)])
         for i in range(b - 1):
             working_block[unknown_byte_size - b + i] = matches[i][matches_index[i]]
         d = encryption_oracle(bytes(working_block[:-b]), key)
-        byte_to_find = d[unknown_byte_size - 1]
+        # TODO assume we can len of prefix,
+        byte_find_index = len(prefix_bin) + len(working_block) - 1
+        byte_to_find = d[byte_find_index]
 
         for i in range(0, MAX_ASCII):
             c = (i + 32) % MAX_ASCII  # prio control codes last
             working_block[-1] = c
             d = encryption_oracle(bytes(working_block), key)
-            if d[unknown_byte_size - 1] == byte_to_find:
+            if d[byte_find_index] == byte_to_find:
                 matches[b - 1].append(c)
 
         if len(matches[b - 1]) == 0:
@@ -77,12 +77,12 @@ def find_all_bytes(key_size, key, unknown_byte_size_in, offset):
         else:
             b += 1
 
-    return "".join([chr(matches[i][matches_index[i]]) for i in range(unknown_byte_size)])
+    final_str = "".join([chr(matches[i][matches_index[i]]) for i in range(unknown_byte_size_in)])
+    return final_str
 
 
 def run():
-    # key = get_random_bytes(16)
-    key = str2bin("YELLOW SUBMARINE")
+    key = get_random_bytes(16)
     # find keysize
     key_size, offset = find_key_size(key)
     print("key size & offset:", key_size, offset)
