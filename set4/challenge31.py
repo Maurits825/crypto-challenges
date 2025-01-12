f89354df4cc5b07aabc83d27e245f5b2c660ced4
Maurits <63048736+Maurits825@users.noreply.github.com>
2025-01-12 19:54:11 +0100

    c31

--- set4/challenge31.py
+++ set4/challenge31.py
@@ -40,7 +40,7 @@ class HMACServer:
             if b1 != b2:
                 return False
             if self.is_sim_time:
-                self.sim_time += (self.sleep_time * (random.randint(80, 120) / 100))
+                self.sim_time += (self.sleep_time * (random.randint(99, 101) / 100))
             else:
                 time.sleep(self.sleep_time)
 
@@ -48,18 +48,40 @@ class HMACServer:
 
 
 def attack(hmac_server):
-    h = hmac_server.generate_hash(b"foo")
-    size = len(h)
+    valid_hash = hmac_server.generate_hash(b"")
+    size = len(valid_hash)
     message = b"some message"
     target_h = hmac_server.generate_hash(message)
     forged_hash = bytearray(size)
+    test_h = bytearray(size)
     count = 1
+    test_count = 10
     for i in range(size):
-        diffs = []
-        forged_hash[i:] = get_random_bytes(size - i)
+        average_t = 0
+        test_h[:] = valid_hash
+        if i + 1 < size:
+            test_h[i + 1] ^= 1
+        for _ in range(test_count):
+            start = hmac_server.get_time()
+            _ = hmac_server.validate_hash(b"", test_h)
+            end = hmac_server.get_time()
+            diff = end - start
+            average_t += diff
+        average_t /= test_count
+        average_byte_sleep = average_t / (i + 1)
+        tolerance = abs((1 / (2 * i - 1)) * 0.8)
+        threshold = average_t - (tolerance * average_byte_sleep)
+
+        eta = 0
+        for j in range(i, size):
+            eta += (256 / 2) * average_byte_sleep * j
+
+        print("Byte/Tries", i, count, int(eta))
+        highest_t = 0
+        highest_b = 0
         for b in range(0, 256):
             forged_hash[i] = b
-            print("Trying", count, forged_hash, target_h)
+            # print("Trying", count, forged_hash, target_h)
             start = hmac_server.get_time()
             is_valid = hmac_server.validate_hash(message, forged_hash)
             end = hmac_server.get_time()
@@ -67,17 +89,25 @@ def attack(hmac_server):
                 break
             count += 1
             diff = end - start
-            # this doenst work if the next byte happens to be 0x00
+            if diff > highest_t:
+                highest_t = diff
+                highest_b = b
             d = diff - ((i + 1) * 0.05)
-            dd = d ** 2
-            if d > 0.001 and b > 5:
+            d2 = (diff - average_t) / average_t
+            if diff > threshold:
                 # print("Found", i, bytes(forged_hash), target_h)
-                break
-            diffs.append(diff)
+                if b != target_h[i]:
+                    print("incorrect match", highest_t, average_t, threshold)
+                else:
+                    break
+
         else:
             print("Didnt find byte", i, bytes(forged_hash), target_h, hmac_server.key.hex())
-            return
-        diffs.sort()
+            print("Trying Best guess", chr(highest_b), chr(target_h[i]), highest_t, average_t, threshold)
+            if highest_b != target_h[i]:
+                print("Bad guess")
+                return
+            forged_hash[i] = highest_b
 
     is_valid = hmac_server.validate_hash(message, forged_hash)
     print("Forged hash:", is_valid, count, bytes(forged_hash), message, hmac_server.key.hex())
@@ -85,8 +115,11 @@ def attack(hmac_server):
 
 def run():
     for i in range(1):
-        server = HMACServer(is_sim_time=True, sleep_time=0.05)
+        is_sim = True
+        server = HMACServer(is_sim_time=is_sim, sleep_time=0.001)
         attack(server)
+        if is_sim:
+            print("Server time min", server.get_time() // 60)
 
 
 if __name__ == "__main__":
